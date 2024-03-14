6a006afcef889b366c8a8d4d938f8312a32634e5
12eu8oed <150359214+12eu8oed@users.noreply.github.com>
2024-03-15 00:50:28 +0900

    Make using event in Component aka 사용자 정의 태그

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import logo from './logo.svg';
 import './App.css';  //앱 디자인
-function Header(props) { //사용자 정의 태그를 만들때는 첫글자 대문자로 upper 이것이 바로 !!컴포넌트!!
-  console.log('props: ', props, props.title);
-  return <header>
-    <h1><a href="/">{props.title}</a></h1>
-  </header>
-
-}
 
 function Article(props) {
   return <article>
@@ -15,12 +8,31 @@ function Article(props) {
   </article>
 }
 
+function Header(props) { //사용자 정의 태그를 만들때는 첫글자 대문자로 upper 이것이 바로 !!컴포넌트!!
+  // console.log('props: ', props, props.title);
+  return <header>
+    <h1><a href="/" onClick={(event) => {  // 화살표 함수란, `function() {   }` 를 `() => {   }` 이런식으로 똑같다 동작이!
+      event.preventDefault();  // 함수를 클릭하고 실행된 후 창이 새로고침되는 것 방지!!
+      props.onChangeMode();    // App()에 있는 Header태그 부분에 props로 들어가있는 onChangeMode()를 실행 시켜준다!
+    }} >{props.title}</a></h1>
+  </header>
+
+}
+
 function Nav(props) {
   const lis = [];
-  for(let i=0; i<props.topics.length; i++) {
+  for (let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];
-    console.log("topics 동적 html 그리기",t);
-    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+    console.log("topics 동적 html 그리기", t);
+
+    lis.push(
+      <li key={t.id}>  {/* 키 값으로 index처럼 주는 느낌! 고유 키*/}
+        <a id={t.id} href={'/read/' + t.id} onClick={event=>{
+          event.preventDefault();  // 클릭시 새로고침 방지
+          props.onChangeMode(event.target.id); //target은 이벤트를 유발시킨 태그를 가리킨다! 여기서는 <a> 태그
+        }}>{t.title}</a>  {/*파라미터값이 하나일때는 괄호 생략가능함! (event) => {} */} 
+      </li>
+    )
   }
 
   return <nav>
@@ -39,8 +51,12 @@ function App() {
 
   return (
     <div>
-      <Header title="WEB"></Header>   {/* This is Component!! */}
-      <Nav topics={topics}></Nav>                       {/* This is Component!! */}
+      <Header title="WEB" onChangeMode={() => {   //Header태그를 클릭했을 때 function 안에 기능을 실행
+        alert("이벤트 입니다~~~");
+      }}></Header>   {/* This is Component!! */}
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>                       {/* This is Component!! */}
       <Article title="Welcome" body="Hello, WEB"></Article>               {/* This is Component!! */}
     </div>
 
