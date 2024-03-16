17d545b9043bf442b4f3b2733641d657fb2a095a
12eu8oed <150359214+12eu8oed@users.noreply.github.com>
2024-03-16 18:10:44 +0900

    Event part

--- src/App.js
+++ src/App.js
@@ -19,18 +19,18 @@ function Header(props) { //사용자 정의 태그를 만들때는 첫글자 대
 
 }
 
-function Nav(props) {
-  const lis = [];
+function Nav(props) { // Nav라는 이름을 가진 사용자 정의 태그(Component) props를 인자로 받는다.
+  const lis = []; // lis라는 이름을 가진 빈 배열을 만든다. 여기에 동적으로 push()함수를 이용해서 html출력할 예정
   for (let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];
     console.log("topics 동적 html 그리기", t);
 
     lis.push(
       <li key={t.id}>  {/* 키 값으로 index처럼 주는 느낌! 고유 키*/}
-        <a id={t.id} href={'/read/' + t.id} onClick={event=>{
+        <a id={t.id} href={'/read/' + t.id} onClick={event => {
           event.preventDefault();  // 클릭시 새로고침 방지
           props.onChangeMode(event.target.id); //target은 이벤트를 유발시킨 태그를 가리킨다! 여기서는 <a> 태그
-        }}>{t.title}</a>  {/*파라미터값이 하나일때는 괄호 생략가능함! (event) => {} */} 
+        }}>{t.title}</a>  {/*파라미터값이 하나일때는 괄호 생략가능함! (event) => {} */}
       </li>
     )
   }
@@ -43,21 +43,31 @@ function Nav(props) {
 }
 
 function App() {
+  const mode = 'WELCOME'; //mode안에 값에 따라 출력되는 데이터가 다르게 나오게 만들 예정
   const topics = [ //동적으로 그려줄 데이터
     { id: 1, title: 'html', body: 'html is ...' }
     , { id: 2, title: 'css', body: 'css is ...' }
     , { id: 3, title: 'javascript', body: 'javascript is ...' }
   ]
-
+  let content = null; //mode값에 따라 출력되는 content 일단 null으로 선언
+  if (mode === 'WELCOME') {
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  }
+  else if (mode === 'READ') {
+    content = <Article title="Read" body="Hello, Read"></Article>
+  }
   return (
     <div>
       <Header title="WEB" onChangeMode={() => {   //Header태그를 클릭했을 때 function 안에 기능을 실행
         alert("이벤트 입니다~~~");
-      }}></Header>   {/* This is Component!! */}
-      <Nav topics={topics} onChangeMode={(id)=>{
+      }}>
+      </Header>   {/* This is Component!! */}
+
+      <Nav topics={topics} onChangeMode={(id) => {
         alert(id);
-      }}></Nav>                       {/* This is Component!! */}
-      <Article title="Welcome" body="Hello, WEB"></Article>               {/* This is Component!! */}
+      }}>
+      </Nav>                       {/* This is Component!! */}
+      {content} {/* content안에 들어가는 Component를 동적으로 출력 */}
     </div>
 
     // <div className="App">
